c01ca6abe8af422eba7af4c87fec9ff9f2884b83
Dmitriy Veshnyakov <dmitriyvs85@mail.ru>
2021-10-07 08:31:35 +0700

    Добавление статусов к статьям и комментариям

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+# модель Статьи
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# модель Комментария
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
 end
